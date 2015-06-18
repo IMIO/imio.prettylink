74ee69154b2eee181ad0192695e926467d8bbd01
Gauthier Bastien <g.bastien@imio.be>
2015-06-18 09:09:31 +0200

    Completed docstring in interfaces.py

--- src/imio/prettylink/interfaces.py
+++ src/imio/prettylink/interfaces.py
@@ -9,5 +9,18 @@ class IPrettyLink(Interface):
     def getLink(self):
         """Returns complete link to the element."""
 
-    def _leadingIcons(self):
+    def CSSClasses(self):
+        """Manage CSS classes to apply to the link.
+           It depends on :
+           - additionalCSSClasses : some arbitrary classes you can define;
+           - showColors : will display a colored link depending on workflow state;
+           - showContentIcon : will apply the class that displays the content icon."""
+
+    def _icons(self):
         """Returns icons to prepend to the link."""
+
+    def _leadingIcons(self):
+        """Returns icons to prepend to icons returned by _icons."""
+
+    def _trailingIcons(self):
+        """Returns icons to append to icons returned by _icons."""
